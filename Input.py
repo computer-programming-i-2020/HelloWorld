ce941b86812dac987d251976d737f2733acb0ef6
Karl Peterson <kpeters4@ncsu.edu>
2019-06-20 03:09:37 -0400

    Create Input.py
    Added basic introduction to getting user input

---
+++ Input.py
@@ -0,0 +1,13 @@
+'''
+Created on Jun 20, 2019
+
+@author: karl
+'''
+
+#There are functions that you can use to let the user input information to the program
+input("Type something ") #This lets you add a string parameter as a prompt for the input
+
+#This can be used to set the value of variables
+x = input("Type something ")
+
+print(x)
